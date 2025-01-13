e696130b5ca2156430804da983fdeea73d4ba438
jvf349 <92024287+jvf349@users.noreply.github.com>
2025-01-13 17:50:06 +0100

    cambios 0 en clase hibernateutil

--- AD03/HibernateUtil.java
+++ AD03/HibernateUtil.java
@@ -5,11 +5,12 @@ public class HibernateUtil {
     // Código estático. Sólo se ejecuta una vez, como un Singleton
     static {
         try {
-            // Creamos es SessionFactory desde el fichero hibernate.cfg.xml 
+            // Creamos el SessionFactory desde el archivo estándar de configuración (hibernate.cfg.xml)
             sessionFactory = new Configuration()
                 .configure(new File("hibernate.cfg.xml")).buildSessionFactory();    
-        } catch (Throwable ex) {
-            System.err.println("Error en la inicialización.  " + ex);
+        } catch (HibernateException  ex) {
+            // Log the exception.
+            System.err.println("Error en la inicialización de SessionFactory." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
